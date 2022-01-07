4abfc9a78cd9974622ed7babd49f790f1c9f40e5
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-01-07 11:43:44 -0300

    expondo os dados e forma dinâmica

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,58 +2,113 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.scss";
-import {database} from '../services/firebase'
-import { FormEvent, useState } from "react";
+import { database } from "../services/firebase";
+import { FormEvent, useState, useEffect } from "react";
+
+type contato = {
+  chave: string;
+  nome: string;
+  email: string;
+  contato: string;
+  observacao: string;
+};
 
 const Home: NextPage = () => {
- const [nome, setNome] = useState('')
- const [email, setEmail] = useState('')
- const [contato, setContato] =  useState('')
- const [observacao, setObservacao] = useState('')
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [contato, setContato] = useState("");
+  const [observacao, setObservacao] = useState("");
 
- console.log(nome, email, contato, observacao)
+  const [contatos, setContatos] = useState<contato[]>();
 
- function gravar(event: FormEvent){
-   event.preventDefault()
-   const ref = database.ref('contatos')
+  useEffect(() => {
+    const refContatos = database.ref("contatos");
 
-   const dados = {
-     nome,
-     email,
-     contato,
-     observacao
-   }
-   ref.push(dados)
- }
+    refContatos.on("value", (resultado) => {
+      const resultadoContatos = Object.entries<contato>(
+        resultado.val() ?? {}
+      ).map(([chave, valor]) => {
+        return {
+          chave: chave,
+          nome: valor.nome,
+          email: valor.email,
+          contato: valor.contato,
+          observacao: valor.observacao,
+        };
+      });
 
+      setContatos(resultadoContatos);
+    });
+  }, []);
+
+  function gravar(event: FormEvent) {
+    event.preventDefault();
+    const ref = database.ref("contatos");
+
+    const dados = {
+      nome,
+      email,
+      contato,
+      observacao,
+    };
+    ref.push(dados);
+
+    setNome("");
+    setEmail("");
+    setContato("");
+    setObservacao("");
+  }
 
   return (
     <>
       <main className={styles.container}>
         <form onSubmit={gravar}>
           <h2>Adicione um contato</h2>
-          <input type="text" placeholder="Nome" onChange={event => setNome(event.target.value)}></input>
-          <input type="email" placeholder="Email" onChange={event => setEmail(event.target.value)} ></input>
-          <input type="tel" placeholder="Contato" onChange={event => setContato(event.target.value)} ></input>
-          <textarea placeholder="Observações"  onChange={event => setObservacao(event.target.value)}/>
+          <input
+            type="text"
+            placeholder="Nome"
+            value={nome}
+            onChange={(event) => setNome(event.target.value)}
+          ></input>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          ></input>
+          <input
+            type="tel"
+            placeholder="Contato"
+            value={contato}
+            onChange={(event) => setContato(event.target.value)}
+          ></input>
+          <textarea
+            placeholder="Observações"
+            value={observacao}
+            onChange={(event) => setObservacao(event.target.value)}
+          />
           <button type="submit">Salvar</button>
         </form>
         <div className={styles.contatos}>
           <input type="text" placeholder="Buscar"></input>
-          <div className={styles.contatoIndividual}>
-            <div className={styles.box}>
-              <p className={styles.nome}>Nome</p>
-              <div>
-                <a>Editar</a>
-                <a>Excluir</a>
+          {contatos?.map((contato) => {
+            return(
+              <div className={styles.contatoIndividual}>
+              <div className={styles.box}>
+                <p className={styles.nome}>{contato.nome}</p>
+                <div>
+                  <a>Editar</a>
+                  <a>Excluir</a>
+                </div>
               </div>
-            </div>
-            <div className={styles.informacao}>
-              <p>E-mail</p>
-              <p>Contato</p>
-              <p>tel</p>
-            </div>
-          </div>
+              <div className={styles.informacao}>
+                <p>{contato.email}</p>
+                <p>{contato.contato}</p>
+                <p>{contato.observacao}</p>
+              </div>
+            </div>;
+            );            
+          })}
         </div>
       </main>
     </>
